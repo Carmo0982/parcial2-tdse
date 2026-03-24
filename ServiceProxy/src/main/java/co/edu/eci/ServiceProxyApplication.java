d6310c3b0ef3e74d13534fdf289c6784c07cb05f
Santiago Carmona <santiago.carmona-p@mail.escuelaing.edu.co>
2026-03-24 16:45:08 -0500

    feat: configuracion inicial

---
+++ ServiceProxy/src/main/java/co/edu/eci/ServiceProxyApplication.java
@@ -0,0 +1,11 @@
+package co.edu.eci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProxyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProxyApplication.class, args);
+    }
+}
